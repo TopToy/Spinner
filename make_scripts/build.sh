05a702e39f2037897b53587f37f01837eb39a069
Yehonatan Buchnik <yontyon@gmail.com>
2019-06-24 19:02:57 +0300

    Add client rpc module

--- make_scripts/build.sh
+++ make_scripts/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+#pip install protobuf
+#pip install grpcio-tools
+#pip install grpcio
+#pip install django
+#pip install djangorestframework
+
 protoc --proto_path=proto --python_out=src/proto proto/**/*.proto
 python -m grpc_tools.protoc --proto_path=proto --python_out=src/proto --grpc_python_out=src/proto proto/crpcs/*.proto
---
+++ src/rpcs/TTClient.py
@@ -0,0 +1,20 @@
+import grpc
+from proto.crpcs import clientService_pb2_grpc
+from utils.loggingUtils import get_logger
+
+
+class TTClient(object):
+
+    def __init__(self, serverIP, serverPort=9876):
+        self.logger = get_logger()
+        self.logger.info('connect to %s:%d'.format(serverIP, serverPort))
+        self.channel = grpc.insecure_channel('%s:%d'.format(serverIP, serverPort))
+        self.stub = clientService_pb2_grpc.ClientServiceStub(self.channel)
+
+    def shutdown(self):
+        self.logger.info('Shutdown')
+        self.channel.close()
+
+    def get_stub(self):
+        return self.stub
+
