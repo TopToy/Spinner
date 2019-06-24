c3357ad4f2edbf80e94a3b832ff4ba2a6e3f83a3
Yehonatan Buchnik <yontyon@gmail.com>
2019-06-24 19:28:03 +0300

    edit urls and models

--- src/TTRest/TTR/models.py
+++ src/TTRest/TTR/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Integer(models.Model):
+    val = models.IntegerField(default=-1)
+
+
+class TxId(models.Model):
+    pid = models.IntegerField(default=-1)
+    bid = models.IntegerField(default=-1)
+    txNum = models.IntegerField(default=-1)
+    worker = models.IntegerField(default=-1)
+
+    def __str__(self):
+        return '[%d%d%d%d]'.format(self.worker, self.pid, self.bid, self.txNum)
