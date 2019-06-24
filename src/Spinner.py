93d5441163fc35269003142026c19e91cb4e3711
Yehonatan Buchnik <yontyon@gmail.com>
2019-06-24 16:32:40 +0300

    First commit
    prepare the environment

---
+++ src/Spinner.py
@@ -0,0 +1,10 @@
+from proto.spinnerTypes.transaction_pb2 import Transaction
+from proto.spinnerTypes.utils_pb2 import Empty
+
+
+def main():
+    msg = Empty()
+    tx = Transaction()
+
+if __name__ == '__main__':
+    main()
